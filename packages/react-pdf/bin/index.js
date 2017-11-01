90f0851db325ec8c1faf02a36fcfe2b1b9a20614
Raphael Saunier <raphael.saunier@gmail.com>
2017-11-01 11:21:23 +0100

    Add RHEL-compatible nbind.node binary into 6-rhel folder

--- packages/react-pdf/bin/index.js
+++ packages/react-pdf/bin/index.js
@@ -1,6 +1,8 @@
+const os = require('os');
 const nbind = require('nbind');
 
 const nodeVersion = process.version.match(/^v(\d+)/)[1];
-const ret = nbind.init(__dirname + '/' + nodeVersion);
+const isRHEL = /el7/.test(os.release());
+const ret = nbind.init(__dirname + '/' + nodeVersion + (isRHEL ? '-rhel' : ''));
 
 module.exports = require('./entry-common')(ret.bind, ret.lib);
